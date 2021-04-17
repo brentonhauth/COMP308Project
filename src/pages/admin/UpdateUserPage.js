a1371afe4906033e853c06913692af5ec56ceae0
HyungseokLeee <hyngsuki@gmail.com>
2021-04-17 05:36:32 -0400

    Changed create/edit user forms

--- src/pages/admin/UpdateUserPage.js
+++ src/pages/admin/UpdateUserPage.js
@@ -58,35 +58,51 @@ class UpdateUserPage extends React.Component {
         <hr></hr>
         <FormWrapper onSubmit={this.tryUpdate}>
           {sending => <>
-            <p className="tableSection">
-              Personal Information
-            </p>
             <p className="double">
               <label htmlFor="firstName" className="floatLabel" type="text">First Name</label>                
-              <input type="text" defaultValue={user.firstName} name="firstName" required/>
+              <input type="text" placeholder="John" name="firstName" defaultValue={user.firstName} required/>
 
               <label htmlFor="lastName" className="floatLabel" type="text">Last Name</label>                
-              <input type="text" defaultValue={user.lastName} name="lastName" required/>
+              <input type="text" placeholder="Doe" name="lastName" defaultValue={user.lastName} required/>
             </p>
             <p>
               <label htmlFor="email" className="floatLabel" type="text">Email</label>                
-              <input type="text" defaultValue={user.email} name="email" disabled/>
+              <input type="text" placeholder="someone@example.com" name="email"defaultValue={user.email} required/>
+            </p>
+            <hr></hr>
+            <p>
+              <label htmlFor="height" className="floatLabel" type="text">Height(CM)</label>                
+              <input type="number" placeholder="180" name="height"defaultValue={user.height} required/>
             </p>
             <p>
-              <label htmlFor="role" className="floatLabel" type="text">Role</label>                
-              <input type="text" defaultValue={user.role} name="role" required/>
+              <label htmlFor="waist" className="floatLabel" type="text">Waist(KG)</label>                
+              <input type="number" placeholder="80" name="waist"defaultValue={user.waist} required/>
+            </p>
+            <hr></hr>
+            <p className="tableSection">
+              Age
+            </p>
+            <p className="double">
+              <label htmlFor="age" className="floatLabel" type="text">Age</label>                
+              <input type="number" placeholder="" name="age"defaultValue={user.age} required/>
+
+              <label htmlFor="gender" className="floatLabel" type="text">Gender</label>
+              {
+                user.gender === 'M'?
+                <select name="gender" id="gender" defaultValue={'M'}>
+                <option value= 'M'>Male</option>
+                <option value='F'>Female</option>
+                </select>
+                :
+                <select name="gender" id="gender" defaultValue={'F'}>
+                <option value= 'M'>Male</option>
+                <option value='F'>Female</option>
+                </select>      
+              }
             </p>
             <p>
               <label htmlFor="password" className="floatLabel" type="text">Password</label>                
-              <input type="text" defaultValue={user.password} name="password" required/>
-            </p>
-            <p>
-              <label htmlFor="age" className="floatLabel" type="text">Age</label>                
-              <input type="text" defaultValue={user.age} name="age" required/>
-            </p>
-            <p>
-              <label htmlFor="gender" className="floatLabel" type="text">Gender</label>                
-              <input type="text" defaultValue={user.gender} name="gender" required/>
+              <input type="password" placeholder="*******" name="password" defaultValue={user.password}required/>
             </p>
             <hr></hr>
             <button type="submit">Update</button>
