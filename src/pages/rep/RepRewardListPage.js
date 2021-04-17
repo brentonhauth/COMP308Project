1a62ef47d7961ac0151b7451c39113abd76cb208
HyungseokLeee <hyngsuki@gmail.com>
2021-04-17 01:50:30 -0400

    Implementing create rewards

--- src/pages/rep/RepRewardListPage.js
+++ src/pages/rep/RepRewardListPage.js
@@ -2,15 +2,68 @@ import UserRole from '../../types/UserRole';
 import React from 'react';
 import EnsureLoggedIn from '../../components/EnsureLoggedIn';
 import { connect } from 'react-redux';
+import * as api from '../../api/CustomerRep';
+import Toast from "../../helpers/Toast";
+import Loading from "../../components/Loading";
+import { Link } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import SortTable from '../../components/SortTable';
 
 class $RepRewardListPage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {rewards:[]};
+    this.tryGettingRewards = this.tryGettingRewards.bind(this);
   }
 
+  async tryGettingRewards()
+  {
+    try
+    {
+      await api.getAllRewards().then(rewards => 
+        {
+          this.setState({ rewards: rewards });
+          console.log(rewards);
+        });
+    }
+    catch(e)
+    {
+      Toast.error(e);
+    }
+  }
+  componentDidMount() {
+    this.tryGettingRewards();
+  }
   render() {
-    return null;
+    const rewards = this.state.rewards;
+    return (
+      <Container>
+        <h3>All rewards
+          <Link
+            className="btn btn-info"
+            to="/rep/rewards/create"
+          >CREATE</Link>
+        </h3>
+        <SortTable data={rewards}>
+          <SortTable.Col heading="ID" sort="_id">
+            {c => <small>{c._id}</small>}
+          </SortTable.Col>
+          <SortTable.Col heading="Title" sort="title">
+            {c => c.title}
+          </SortTable.Col>
+          <SortTable.Col heading="Company" sort="company">
+            {c => c.company}
+          </SortTable.Col>
+          <SortTable.Col heading="Description" sort="description">
+            {c => c.description}
+          </SortTable.Col>
+          <SortTable.Col>
+            {c => <Link to={`/rep/rewards/${c._id}`} className="btn btn-sm rounded-pill btn-warning">Edit</Link>}
+          </SortTable.Col>
+        </SortTable>
+      </Container>
+    );
+
   }
 }
 
